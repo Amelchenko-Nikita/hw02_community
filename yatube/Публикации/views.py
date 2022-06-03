e3c7538fb513e08789b9b74c22a4e9024ca044cd
Amelchenko-Nikita <104903272+Amelchenko-Nikita@users.noreply.github.com>
2022-06-03 17:51:08 +0500

    Update views.py

--- yatube/Публикации/views.py
+++ yatube/Публикации/views.py
@@ -17,7 +17,7 @@ def index(request):
 
 def group_posts(request, slug):
     date = Meta.ordering
-    posts = Post.objects.filter(grop=group).order_by(date)
+    posts = Post.objects.filter(group = group).order_by(date)
     group = get_object_or_404(Group, slug=slug)
     context = {
         'POSTS_PER_PAGE': settings.POSTS_PER_PAGE,
