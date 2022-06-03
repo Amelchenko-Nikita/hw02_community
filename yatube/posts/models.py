bb404bb104659552a2993df0de4ba87ca202e1cb
Amelchenko-Nikita <104903272+Amelchenko-Nikita@users.noreply.github.com>
2022-06-03 21:35:22 +0500

    Update models.py

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,13 +1,14 @@
-from django.db import models
-from django.contrib.auth import get_user_model
+from django.db import models 
+from django.contrib.auth import get_user_model 
 
+ 
 # Create your models here.
 
 User = get_user_model()
 
 
 class Post(models.Model):
-    text = models.TextField(verbose_name='texts')
+    text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
@@ -15,18 +16,12 @@ class Post(models.Model):
         related_name='posts'
     )
     group = models.ForeignKey(
-        'Group', on_delete=models.SET_NULL, related_name='group_posts',
+        'Group', on_delete=models.CASCADE, related_name="group_posts",
         blank=True, null=True
     )
-
-    class Meta:
-        ordering = ('-pub_date')
-
-
 class Group(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
-
     def __str__(self):
         return self.title
