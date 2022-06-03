a0944ba275b1c8b1393a8b6bc0552003e52d4904
Amelchenko-Nikita <104903272+Amelchenko-Nikita@users.noreply.github.com>
2022-06-03 21:40:44 +0500

    Update models.py

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 
-#Create your models here.
+# Create your models here.
 
 User = get_user_model()
 
@@ -19,12 +19,12 @@ class Post(models.Model):
         'Group', on_delete=models.CASCADE, related_name="group_posts",
         blank=True, null=True
     )
-    
-    
+
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
-    
+
     def __str__(self):
         return self.title
