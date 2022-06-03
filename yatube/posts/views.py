c4b82c584a251ec22da2c51f377ea37fa0b72a48
Amelchenko-Nikita <104903272+Amelchenko-Nikita@users.noreply.github.com>
2022-06-03 15:13:22 +0500

    Update views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -9,7 +9,7 @@ def index(request):
     posts = Post.objects.order_by(date)
     context = {
         'POSTS_PER_PAGE': settings.POSTS_PER_PAGE,
-        'posts':posts
+        'posts': posts
     }
     return render(request, 'posts/index.html', context)
 
