bb3c0263c02468ae3a8112188e4c7772aa30df96
Amelchenko-Nikita <104903272+Amelchenko-Nikita@users.noreply.github.com>
2022-06-03 19:42:07 +0500

    Update models.py

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -18,8 +18,8 @@ class Post(models.Model):
         'Group', on_delete=models.SET_NULL, related_name='group_posts',
         blank=True, null=True
     )
-     class Meta:
-         ordering = '(-pub_date)'
+    class Meta:
+        ordering = '(-pub_date)'
 
 
 class Group(models.Model):
