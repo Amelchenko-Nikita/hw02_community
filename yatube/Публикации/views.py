4b6a6d67a1f9470206b3184509a0141d9acb5ce0
Amelchenko-Nikita <104903272+Amelchenko-Nikita@users.noreply.github.com>
2022-06-03 17:55:50 +0500

    Update views.py

--- yatube/Публикации/views.py
+++ yatube/Публикации/views.py
@@ -7,9 +7,8 @@ from .models import Post, Group, Meta
 
 def index(request):
     date = Meta.ordering
-    posts = Post.objects.order_by(date)
+    posts = Post.objects.order_by(date)[:10]
     context = {
-        'POSTS_PER_PAGE': settings.POSTS_PER_PAGE,
         'posts': posts
     }
     return render(request, 'posts/index.html', context)
@@ -17,10 +16,9 @@ def index(request):
 
 def group_posts(request, slug):
     date = Meta.ordering
-    posts = Post.objects.filter(group = group).order_by(date)
+    posts = Post.objects.filter(group=group).order_by(date)[:10]
     group = get_object_or_404(Group, slug=slug)
     context = {
-        'POSTS_PER_PAGE': settings.POSTS_PER_PAGE,
         'group': group,
         'posts': posts,
     }
