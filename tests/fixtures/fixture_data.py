98f345aa65c70fd91056e2088dc946ef0253fca2
Amelchenko-Nikita <104903272+Amelchenko-Nikita@users.noreply.github.com>
2022-06-03 21:03:30 +0500

    Update fixture_data.py

--- tests/fixtures/fixture_data.py
+++ tests/fixtures/fixture_data.py
@@ -1,17 +1,35 @@
-import pytest
-from posts.models import Group, Post
+import pytest 
 
+from posts.models import Group, Post 
 
-@pytest.fixture
-def post(user):
-    return Post.objects.create(text='Тестовый пост 1', author=user)
+ 
 
+ 
 
-@pytest.fixture
-def group():
-    return Group.objects.create(title='Тестовая группа 1', slug='test-link', description='Тестовое описание группы')
+@pytest.fixture 
 
+def post(user): 
 
-@pytest.fixture
-def post_with_group(user, group):
-    return Post.objects.create(text='Тестовый пост 2', author=user, group=group)
+    return Post.objects.create(text='Тестовый пост 1', author=user) 
+
+ 
+
+ 
+
+@pytest.fixture 
+
+def group(): 
+
+    return Group.objects.create(title='Тестовая группа 1', slug='test-link', description='Тестовое описание группы') 
+
+ 
+
+ 
+
+@pytest.fixture 
+
+def post_with_group(user, group): 
+
+    return Post.objects.create(text='Тестовый пост 2', author=user, group=group) 
+
+ 
