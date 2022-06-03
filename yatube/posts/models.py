ba2db1e91f62bf67acca7c60da3504597c5e5260
Amelchenko-Nikita <104903272+Amelchenko-Nikita@users.noreply.github.com>
2022-06-03 14:56:16 +0500

    Update models.py

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 
 class Post(models.Model):
-    text = models.TextField()
+    text = models.TextField(verbose_name = ' texts ')
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
@@ -15,14 +15,19 @@ class Post(models.Model):
         related_name='posts'
     )
     group = models.ForeignKey(
-        'Group', on_delete=models.CASCADE, related_name="group_posts", blank=True, null=True
+        'Group', on_delete=models.SET_NULL, related_name='group_posts',
+        blank=True, null=True
     )
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(unique=True)
     description = models.TextField()
 
     def __str__(self):
         return self.title
+
+
+class Meta:
+        ordering = '-pub_date'
