f34d667861eb713c25ac8e73eec0e61b943b1a12
Amelchenko-Nikita <104903272+Amelchenko-Nikita@users.noreply.github.com>
2022-06-03 20:44:51 +0500

    Update views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -16,7 +16,7 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).ordering[:10]
     context = {
-        'posts': posts,
         'group': group,
+        'posts': posts,
     }
     return render(request, 'posts/group_list.html', context)
