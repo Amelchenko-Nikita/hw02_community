431e63a6a1478b301e92cde438b8153b572deacc
Amelchenko-Nikita <104903272+Amelchenko-Nikita@users.noreply.github.com>
2022-06-03 21:14:42 +0500

    Update views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,30 +1,28 @@
-from django.shortcuts import render, get_object_or_404 
-from .models import Post, Group 
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
  
 
 # Create your views here 
 
 
-def index(request): 
-    posts = Post.objects.order_by('-pub_date')[:10] 
-    template = 'posts/index.html' 
-    context = { 
-        'text': 'Последние обновления на сайте', 
-        'posts': posts, 
-    } 
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    template = 'posts/index.html'
+    context = {
+        'text': 'Последние обновления на сайте',
+        'posts': posts,
+    }
     return render(request, template, context)
 
 
-def group_posts(request, slug): 
-    group = get_object_or_404(Group, slug=slug) 
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10] 
-    template = 'posts/group_list.html' 
-    context = { 
-
-        'group': group, 
-        'posts': posts, 
-        'text': "Здесь будет информация о группах проекта Yatube", 
-    } 
-
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    template = 'posts/group_list.html'
+    context = {
+        'group': group,
+        'posts': posts,
+        'text': "Здесь будет информация о группах проекта Yatube",
+    }
     return render(request, template, context)
