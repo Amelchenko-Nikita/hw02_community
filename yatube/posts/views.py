e4d1707c6326d94fe0bee31d2c9bd34665c658f1
Amelchenko-Nikita <104903272+Amelchenko-Nikita@users.noreply.github.com>
2022-06-04 13:01:08 +0300

    Update views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,21 +6,17 @@ from .models import Post, Group
 
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
-    template = 'posts/index.html'
     context = {
-        'text': 'Последние обновления на сайте',
         'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-    template = 'posts/group_list.html'
     context = {
         'group': group,
         'posts': posts,
-        'text': "Здесь будет информация о группах проекта Yatube",
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
