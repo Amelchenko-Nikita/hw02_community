3812d7f2bb4b7734e507df0a03ed33af2b0d372d
Amelchenko-Nikita <104903272+Amelchenko-Nikita@users.noreply.github.com>
2022-06-07 08:47:07 +0300

    Update views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,7 +6,7 @@ from .models import Post, Group
 def index(request):
     posts = (Post.objects.select_related('group', 'author')
              [:POSTS_PER_PAGE]
-            )
+             )
     context = {
         'posts': posts,
     }
@@ -17,7 +17,7 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = (Post.objects.select_related('group', 'author')
              .filter(group=group)[:POSTS_PER_PAGE]
-            )
+             )
     context = {
         'group': group,
         'posts': posts,
